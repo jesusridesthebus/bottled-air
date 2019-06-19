3fc1a28b6ad0d25bfa56180d5efea853d6dc43ce
Katlin Anderson and Liz Kelley <katlin@>
2019-06-19 14:26:30 -0700

    correct temp change made to routing while debugging earlier

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,7 +9,7 @@ import { AdminComponent }   from './admin/admin.component';
 const appRoutes: Routes = [ 
     {
       path: '',
-      component: AboutComponent
+      component: SplashComponent
       },
     {
       path: 'about',
